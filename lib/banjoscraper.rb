49b60e9a50b01085e5bc29b10cfc5d9505564536
Riley Slayden <slaydenriley@gmail.com>
2019-12-10 16:58:17 +0000

    Started building high-level of Scraper class

--- lib/banjoscraper.rb
+++ lib/banjoscraper.rb
@@ -1,2 +1,13 @@
+require 'open-uri'
+require 'pry'
+
 class Scraper
+  def self.scrape_catelog_page
+    catelog = Nokogiri::HTML(open(https://www.elderly.com/collections/stelling))
+  end
+
+  def self.scrape_info_page(info_url)
+    info = Nokogiri::HTML(open(info_url))
+  end
+
 end
