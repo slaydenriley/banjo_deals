b5c25440c6cf9919e8e03b71cd7624709251d65f
Riley Slayden <slaydenriley@gmail.com>
2019-12-10 17:17:29 +0000

    Got program to a 'runable' test version

---
+++ lib/cli.rb
@@ -0,0 +1,23 @@
+require_relative "../lib/scraper.rb"
+require 'pry'
+class Cli
+
+  def start
+    Scraper.scrape_catelog_page
+    puts "Hello, welcome to the banjo deals catelog!"
+    puts "Type 'Enter' to view the catelog. Type 'Exit' to leave."
+
+    if gets.strip.downcase == "enter"
+      self.catelog
+    elsif gets.strip.downcase == "exit"
+      exit
+    end
+  end
+
+  def catelog
+    puts "Enter banjo number for more detailed description:"
+    puts "1. Stelling Staghorn With Old Wood Rim and Case - $6,250"
+    puts "2. Stelling Master Flower (1995) -$3,600"
+    puts "...etc."
+  end
+end
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,13 +1,14 @@
 require 'open-uri'
+require 'nokogiri'
 require 'pry'
 
 class Scraper
   def self.scrape_catelog_page
-    catelog = Nokogiri::HTML(open(https://www.elderly.com/collections/stelling))
+    catelog = Nokogiri::HTML(open("https://www.elderly.com/collections/stelling"))
+    binding.pry
   end
 
   def self.scrape_info_page(info_url)
     info = Nokogiri::HTML(open(info_url))
   end
-
 end
